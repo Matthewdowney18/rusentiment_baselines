5882461eca587c38e9c48a7998b25f18d892e2e4
matthewdowney18 <matthew_downey@student.uml.edu>
2018-08-22 22:21:41 -0400

    implementing a sequential lstm classifier

---
+++ LSTM_classifacation.py
@@ -0,0 +1,386 @@
+import csv
+import re
+from collections import defaultdict, Counter
+import functools
+from pathlib import Path
+import vecto.embeddings
+
+from sklearn.preprocessing import LabelEncoder, MinMaxScaler, StandardScaler
+from sklearn.utils import shuffle
+from sklearn.model_selection import KFold
+from sklearn.metrics import f1_score, precision_score, recall_score, accuracy_score, confusion_matrix
+from nltk import word_tokenize, TweetTokenizer
+
+import numpy as np
+import torch
+import torch.nn
+import torch.nn.functional as F
+
+from matplotlib import pyplot
+
+class RNN(torch.nn.Module):
+
+    def __init__(self, input_size, hidden_size, output_size):
+        super(RNN, self).__init__()
+
+        self.hidden_size = hidden_size
+
+        self.i2h = torch.nn.Linear(input_size + hidden_size, hidden_size)
+        self.i2o = torch.nn.Linear(input_size + hidden_size, output_size)
+        self.softmax = torch.nn.LogSoftmax(dim=1)
+
+    def forward(self, input, hidden):
+        combined = torch.cat((input, hidden), 1)
+        hidden = self.i2h(combined)
+        output = self.i2o(combined)
+        output = self.softmax(output)
+        return output, hidden
+
+    def initHidden(self):
+        return torch.zeros(1, self.hidden_size)
+
+
+
+class SmallDeepNet(torch.nn.Module):
+    def __init__(self, embedding_dim, hidden_size, nb_classes):
+        super().__init__()
+
+        self.hidden_size = hidden_size
+        self.nb_classes = nb_classes
+        self.embedding_dim = embedding_dim
+
+        self.classifier = torch.nn.Sequential(
+            torch.nn.Linear(self.embedding_dim, self.hidden_size),
+            torch.nn.ELU(),
+            torch.nn.Linear(self.hidden_size, self.hidden_size),
+            torch.nn.ELU(),
+            torch.nn.Linear(self.hidden_size, self.hidden_size),
+            torch.nn.ELU(),
+            torch.nn.Linear(self.hidden_size, self.nb_classes),
+        )
+        self.criterion = torch.nn.CrossEntropyLoss()
+        self.optimizer = torch.optim.SGD(self.parameters(), lr=0.5,
+                                         momentum=0.9)
+
+    def forward(self, inputs):
+        logits = self.classifier(inputs)
+        outputs = F.softmax(logits, dim=-1)
+
+        return outputs
+
+    def backward(self, y_pred, target):
+        loss = self.criterion(y_pred, target)
+
+        # Zero the gradients
+        self.optimizer.zero_grad()
+
+        # perform a backward pass (backpropagation)
+        loss.backward()
+
+        # Update the parameters
+        self.optimizer.step()
+        return loss
+
+class Data:
+    def __init__(self):
+        self.metadata = dict()
+
+    def load_data_from_dataset(self, mode, labels_mode, embedding_file,
+                                  data_file):
+        samples_train, labels_train, samples_test, labels_test, \
+        metadata = create_data(data_file, mode, labels_mode)
+        self.metadata['dataset'] = metadata
+
+        self.metadata['mode'] = mode
+        self.metadata['labels_mode'] = labels_mode
+
+        print(f'Data train: {len(samples_train)}')
+        print(f'Labels train: {Counter(labels_train)}')
+
+        embeddings = load_embeddings(embedding_file)
+        print(f'Word embeddings: {len(embeddings.vocabulary.lst_words)}')
+        self.metadata['embedding'] = embeddings.metadata
+
+        label_encoder = LabelEncoder()
+        label_encoder.fit(labels_train)
+        print(f'Labels: {label_encoder.classes_}')
+        self.metadata['labels'] = label_encoder.classes_
+
+
+        self.X_train = self.create_data_matrix_embeddings(samples_train,
+                                                       embeddings)
+        self.y_train = label_encoder.transform(labels_train)
+        print(f'Train data: {len(self.X_train)}, {self.y_train.shape}')
+        self.metadata['Train data'] = [len(self.X_train), self.y_train.shape]
+
+        self.X_test = self.create_data_matrix_embeddings(samples_test,
+                                                        embeddings)
+        self.y_test = label_encoder.transform(labels_test)
+        print(f'Test data: {len(self.X_test)}, {self.y_test.shape}')
+        self.metadata['Test data'] = [len(self.X_test), self.y_test.shape]
+
+    def create_data_matrix_embeddings(self, samples, word_embeddings):
+        embeddings_dim = len(word_embeddings.matrix[0])
+        nb_samples = len(samples)
+        X = list()
+
+        nb_empty = 0
+        for i, sample in enumerate(samples):
+            sequence = torch.zeros((20, 1, embeddings_dim), dtype=torch.float)
+            tokens = sample.split(' ')
+            tokens_embeddings = [word_embeddings.get_vector(t) for t in tokens
+                                 if
+                                 word_embeddings.has_word(t)][:20]
+            for i in range(0, len(tokens_embeddings)):
+                sequence[i][0] = torch.from_numpy(tokens_embeddings[i])
+            X.append(sequence)
+            if len(tokens_embeddings) == 0:
+                nb_empty += 1
+
+        print(f'Empty samples: {nb_empty}')
+
+        self.metadata['Empty samples'] = nb_empty
+
+        return X
+
+
+
+def load_embeddings(embedding_file):
+    try:
+        embeddings = vecto.embeddings.load_from_dir(embedding_file)
+
+    except EOFError:
+        print(f'Cannot load: {filename}')
+        embeddings = None
+    return embeddings
+
+
+def create_data(data_file, mode, labels_mode):
+    metadata = dict()
+    data_base_filename = data_file.joinpath(
+        'RuSentiment/rusentiment_random_posts.csv')
+    data_posneg_filename = data_file.joinpath(
+        'RuSentiment/rusentiment_preselected_posts.csv')
+    data_test_filename = data_file.joinpath(
+        'RuSentiment/rusentiment_test.csv')
+
+    samples_base_train, labels_base_train = load_data(
+        data_base_filename)
+    samples_posneg_train, labels_posneg_train = load_data(
+        data_posneg_filename)
+    samples_test, labels_test = load_data(data_test_filename)
+
+    print(
+        f'Data base: {len(samples_base_train)}, '
+        f'{len(labels_base_train)}')
+    print(f'Data posneg: {len(samples_posneg_train)},'
+          f' {len(labels_posneg_train)}')
+    print(f'Data test: {len(samples_test)}, {len(labels_test)}')
+    print(f'Labels: {len(set(labels_base_train))},'
+          f' {len(set(labels_base_train))}, {len(set(labels_test))}')
+
+    if mode == 'base':
+        samples_train = samples_base_train
+        labels_train = labels_base_train
+    elif mode == 'posneg':
+        samples_train = samples_base_train + samples_posneg_train
+        labels_train = labels_base_train + labels_posneg_train
+    elif mode == 'pos':
+        target_class = 'positive'
+        target_samples = \
+            [s for s, l in
+             zip(samples_posneg_train, labels_posneg_train)
+             if l == target_class]
+        target_labels = [target_class] * len(target_samples)
+        samples_train = samples_base_train + target_samples
+        labels_train = labels_base_train + target_labels
+    elif mode == 'neg':
+        target_class = 'negative'
+        target_samples = \
+            [s for s, l in
+             zip(samples_posneg_train, labels_posneg_train)
+             if l == target_class]
+        target_labels = [target_class] * len(target_samples)
+        samples_train = samples_base_train + target_samples
+        labels_train = labels_base_train + target_labels
+    elif mode == 'neutral':
+        target_class = 'neutral'
+        target_samples = \
+            [s for s, l in
+             zip(samples_posneg_train, labels_posneg_train)
+             if l == target_class]
+        target_labels = [target_class] * len(target_samples)
+        samples_train = samples_base_train + target_samples
+        labels_train = labels_base_train + target_labels
+    elif mode == 'posneg_only':
+        samples_train = samples_posneg_train
+        labels_train = labels_posneg_train
+    elif mode == 'replace':
+        nb_replace = len(samples_posneg_train)
+        samples_base_train, labels_base_train = \
+            shuffle(samples_base_train, labels_base_train)
+        samples_train = samples_base_train[
+                        :-nb_replace] + samples_posneg_train
+        labels_train = labels_base_train[
+                       :-nb_replace] + labels_posneg_train
+    elif mode == 'debug':
+        nb_samples_debug = 2000
+        samples_train = samples_base_train[:nb_samples_debug]
+        labels_train = labels_base_train[:nb_samples_debug]
+    elif mode == 'sample':
+        nb_sample = len(samples_posneg_train)
+        samples_base_train, labels_base_train = shuffle(
+            samples_base_train, labels_base_train)
+        samples_train = samples_base_train[:nb_sample]
+        labels_train = labels_base_train[:nb_sample]
+    elif mode == 'sample_posneg':
+        nb_samples_by_classes = Counter(labels_posneg_train)
+
+        samples_train = []
+        labels_train = []
+        for target_class, target_counts in \
+                nb_samples_by_classes.most_common():
+            base_samples_of_target_class = [
+                s for s, l in zip(samples_base_train, labels_base_train)
+                if l == target_class]
+            shuffle(base_samples_of_target_class)
+            base_samples_of_target_class = \
+                base_samples_of_target_class[:target_counts]
+
+            samples_train.extend(base_samples_of_target_class)
+            labels_train.extend(
+                [target_class] * len(base_samples_of_target_class))
+    else:
+        raise ValueError(f'Mode {mode} is unknown')
+
+    if labels_mode == 'base':
+        pass
+    elif labels_mode == 'neg':
+        labels_train = ['rest' if lbl != 'negative' else lbl for lbl in
+                        labels_train]
+        labels_test = ['rest' if lbl != 'negative' else lbl for lbl in
+                       labels_test]
+    elif labels_mode == 'pos':
+        labels_train = ['rest' if lbl != 'positive' else lbl for lbl in
+                        labels_train]
+        labels_test = ['rest' if lbl != 'positive' else lbl for lbl in
+                       labels_test]
+    else:
+        raise ValueError(f'Labels mode {labels_mode} is unknown')
+
+    return samples_train, labels_train, samples_test, labels_test, metadata
+
+
+def load_data(data_file):
+    tokenizer = TweetTokenizer()
+
+    with open(data_file, 'r') as f:
+        reader = csv.DictReader(f)
+
+        samples = []
+        labels = []
+        for row in reader:
+            text = row['text']
+            label = row['label']
+
+            text_tokenized = tokenizer.tokenize(text)
+
+            text_joined = ' '.join(text_tokenized)
+
+            samples.append(text_joined)
+            labels.append(label)
+    return samples, labels
+
+def plot(total_loss):
+    pyplot.plot(total_loss)
+    pyplot.title('loss')
+    pyplot.xlabel('epoch')
+    pyplot.ylabel('loss')
+    pyplot.show()
+
+
+def score_model(classifier, X, y_true, labels):
+    y_pred = torch.zeros(y_true.shape)
+
+    for j, x in enumerate(X):
+        hidden = classifier.initHidden()
+
+        for i in range(x.size()[0]):
+            output, hidden = classifier(x[i], hidden)
+
+        _, y_pred[j] = torch.max(output, 1)
+
+        #print(labels[])
+    if len(set(labels)) == 2:
+        average = 'binary'
+        pos_label = int(np.argwhere(labels != 'rest'))
+    else:
+        average = 'weighted'
+        pos_label = 1
+
+    results = dict()
+    results["accuracy"] = accuracy_score(y_true, y_pred)
+    results["f1"] = f1_score(y_true, y_pred, average=average,
+                             pos_label=pos_label)
+    results["precision"] = precision_score(y_true, y_pred, average=average,
+                                 pos_label=pos_label)
+    results["recall"] = recall_score(y_true, y_pred, average=average,
+                            pos_label=pos_label)
+
+    return results
+
+
+
+def main():
+    results = dict
+    # Directory with data files (data_base.csv, data_posneg.csv, etc)
+    data_file = Path('./')
+
+    # Path to a folder that has embedding file within
+    embedding_file = '/home/mattd/projects/tmp/sentiment/fasttext/'
+
+    data = Data()
+    data.load_data_from_dataset('posneg_only', 'base', embedding_file,
+                                  data_file)
+
+    #scaler = StandardScaler()
+    #scaler.fit(data.X_train)
+    #scaler.fit(data.X_test)
+
+    X_train = data.X_train
+    y_train = torch.tensor(data.y_train, requires_grad = False)
+    X_test = data.X_test
+    y_test = torch.tensor(data.y_test, requires_grad=False)
+
+
+    classifier = RNN(300, 128, 5)
+    criterion = torch.nn.NLLLoss()
+    learning_rate = .001
+
+    for epoch in range(0,2):
+        for i, example in enumerate(X_train):
+            hidden = classifier.initHidden()
+
+            classifier.zero_grad()
+
+            for i in range(example.size()[0]):
+                output, hidden = classifier(example[i], hidden)
+
+            target = torch.tensor([y_train[i]])
+            loss = criterion(output, target)
+            loss.backward()
+
+            # Add parameters' gradients to their values, multiplied by learning rate
+            for p in classifier.parameters():
+                p.data.add_(-learning_rate, p.grad.data)
+
+
+        result = score_model(classifier, X_test, y_test,
+                             data.metadata['labels'])
+        #results['result'] = result
+        #results['metadata'] = data.metadata
+        print(result)
+
+
+if __name__ == '__main__':
+    main()
